627aafc534cd956bc5a7bfdcd22157c436969dbb
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2024-07-31 12:31:52 +0530

    Navbar is fixed

--- src/components/common/NavBar.vue
+++ src/components/common/NavBar.vue
@@ -4,7 +4,7 @@ import { profile } from "../../assets/index.js";
 
 <template>
   <div
-    class="flex justify-start items-center bg-charcoal py-4 px-5 gap-2.5 border-b-solid border-nav_border border-b-2 absolute w-full top-0 left-0"
+    class="flex justify-start items-center bg-charcoal py-4 px-5 gap-2.5 border-b-solid border-nav_border border-b-2 fixed w-full top-0 left-0"
   >
     <img :src="profile" alt="profile_image" class="w-9 h-9" />
     <h5 class="text-base font-medium text-not_white max-md:hidden">Abhay Balakrishnan</h5>
