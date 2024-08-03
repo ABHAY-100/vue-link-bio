df948cf5b54de62c083914fd1d9e2d59b07bf39c
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2024-08-03 21:46:15 +0530

    Add footer

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -3,7 +3,7 @@ import { signature } from "../../assets/index.js";
 </script>
 
 <template>
-  <div class="w-[480px] max-md:w-full mx-auto gap-[40px] flex flex-col font-medium">
+  <div class="w-[480px] max-md:w-full mx-auto gap-[40px] flex flex-col font-medium pb-[228px]">
     <div class="flex flex-col items-center justify-center w-full text-sm text-normal">
         <p class="text-button_text">Thanks for visiting.</p>
         <p class="text-[#545454]">Stay awesome.</p>
