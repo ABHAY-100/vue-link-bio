5c9da31335a52c21f3f9002f0b800a9be1be323f
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2024-08-04 19:39:55 +0530

    Add SEO

--- src/main.js
+++ src/main.js
@@ -3,8 +3,5 @@ import './style.css'
 import App from './App.vue'
 import PrimeVue from "primevue/config";
 
-import { MotionPlugin } from '@vueuse/motion';
-
 const app = createApp(App);
 app.use(PrimeVue, { unstyled: true }).mount('#app')
-app.use(MotionPlugin);
