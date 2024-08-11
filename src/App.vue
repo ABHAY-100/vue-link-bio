0ad1de25ae0163e74986c830ab58b496d0ac53a3
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2024-08-11 20:04:26 +0530

    Add loader

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <script setup>
-import Home from "./views/Home.vue";
+import { ref, onMounted } from 'vue';
+import Home from './views/Home.vue';
+import LoadingSpinner from './components/common/Loading.vue';
+
+const isLoading = ref(true);
+
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 3000);
+});
 </script>
 
 <template>
-  <Home />
+  <div v-if="isLoading">
+    <LoadingSpinner />
+  </div>
+  <div v-else>
+    <Home />
+  </div>
 </template>
---
+++ src/components/common/Loading.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+  <div class="loading-container">
+    <picture>
+        <source srcset="https://fonts.gstatic.com/s/e/notoemoji/latest/1f609/512.webp" type="image/webp">
+        <img src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f609/512.gif" alt="😉" width="52" height="52">
+    </picture>
+  </div>
+</template>
+
+<style scoped>
+.loading-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #101010;
+}
+</style>
