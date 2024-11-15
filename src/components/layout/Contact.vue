0fdbd159d3ad9310028cea3476bcf9af7b1e8af0
ABHAY <133576797+ABHAY-100@users.noreply.github.com>
2024-11-15 08:43:00 +0530

    Update Contact.vue

--- src/components/layout/Contact.vue
+++ src/components/layout/Contact.vue
@@ -37,20 +37,23 @@ const socials = [
     link: "https://www.instagram.com/itsabhaybal",
   },
   {
-    icon: "fa-brands fa-github",
-    link: "https://github.com/ABHAY-100",
+    icon: "fa-brands fa-figma",
+    link: "https://www.figma.com/@itsabhay",
   },
   {
-    icon: "fa-brands fa-codepen",
-    link: "https://codepen.io/itsabhaybal",
+    icon: "fa-brands fa-github",
+    link: "https://github.com/ABHAY-100",
   },
   {
     icon: "fa-solid fa-code",
     link: "https://leetcode.com/u/itsabhaybal",
   },
   {
-    icon: "fa-brands fa-figma",
-    link: "https://www.figma.com/@itsabhay",
+    icon: "fa-brands fa-codepen",
+    link: "https://codepen.io/itsabhaybal",
+  },{
+    icon: "fa-brands fa-discord",
+    link: "https://discordapp.com/users/1099246104060641300",
   },
 ];
 </script>
